eca592647b287cf11e4457e3414a13dfea8cd162
Gabriel Kunz <GabrielKunz97@gmail.com>
2021-04-07 10:26:06 -0300

    feat(Person): add age atribute and exception

--- Program02/src/program02/Person.java
+++ Program02/src/program02/Person.java
@@ -7,10 +7,21 @@ package program02;
 
 public class Person {
     private String name;
+    private int age;
     public void setName(String name){
         this.name = name.toUpperCase();
     }
     public String getName(){
         return this.name;
     }
+    public void setAge(int age) throws Exception{
+        if (age < 0){
+            throw new Exception("Age cannot be negative");
+        }
+        this.age = age;
+    }
+    public int getAge(){
+        return this.age;
+    }
+
 }
--- Program02/src/program02/Program02.java
+++ Program02/src/program02/Program02.java
@@ -1,5 +1,8 @@
 package program02;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Program02 {
 
     public static void main(String[] args) {
@@ -7,6 +10,11 @@ public class Program02 {
         Professor professor01 = new Professor();
         
         person01.setName("gabriel");
+        try {
+            person01.setAge(-1);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
         professor01.setName("margrit");
         professor01.setNumberSiape(12345);
         
