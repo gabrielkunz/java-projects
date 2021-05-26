2974543e83e3275f76d172f081c7ed60c0b274c6
Gabriel Kunz <GabrielKunz97@gmail.com>
2021-05-26 20:46:18 -0300

    Fix bugs in main program and Rotina.java

--- Assignment03/src/assignment03/Assignment03.java
+++ Assignment03/src/assignment03/Assignment03.java
@@ -9,18 +9,27 @@ public class Assignment03 {
         
         try {
             String input_without_accents = rotina.EliminaAcento(user_input);
-            System.out.println(input_without_accents);
+            System.out.println("String without accents: " + input_without_accents);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
         
         try {
             String input_without_despaces = rotina.EliminaEspacoDuplo(user_input);
-            System.out.println(input_without_despaces);
+            System.out.println("String without double spaces or more: " 
+                    + input_without_despaces);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
         
+        try {
+            String new_output = rotina.EliminaAcento(user_input);
+            new_output = rotina.EliminaEspacoDuplo(new_output);
+            System.out.println("String without accents and double spaces: " 
+                    + new_output);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
     
 }
--- Assignment03/src/assignment03/Rotina.java
+++ Assignment03/src/assignment03/Rotina.java
@@ -8,19 +8,28 @@ public class Rotina {
             throw new Exception("String cannot be null.");
         }
         
-        if(input.matches(".*[áàãéèíìóòõúù].*")){
+        if(input.matches(".*[áàâãéèêíìîóòõôúùû].*")){
             output = input.replace("á","a");
             output = output.replace("à","a");
             output = output.replace("ã","a");
+            output = output.replace("â","a");
+            
             output = output.replace("é","e");
             output = output.replace("è","e");
+            output = output.replace("ê","e");
+            
             output = output.replace("í","i");
             output = output.replace("ì","i");
+            output = output.replace("î","i");
+            
             output = output.replace("ó","o");
             output = output.replace("ò","o");
             output = output.replace("õ","o");
+            output = output.replace("ô","o");
+            
             output = output.replace("ú","u");
             output = output.replace("ù","u");
+            output = output.replace("û","u");
         } else {
             output = input;
         }
@@ -33,9 +42,9 @@ public class Rotina {
             throw new Exception("String cannot be null.");
         }
         
-        if(input.matches(".*[  ].*")){
+        if(input.contains("  ")){
             output = input;
-            while(output.matches(".*[  ].*")) {
+            while(output.contains("  ")) {
                 output = output.replace("  "," ");
             }
         } else {
