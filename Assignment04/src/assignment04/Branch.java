3a4869960def1fa2c8c33d957b8915193eca7525
Gabriel Kunz <GabrielKunz97@gmail.com>
2021-06-09 23:23:13 -0300

    Update Branch.java

--- Assignment04/src/assignment04/Branch.java
+++ Assignment04/src/assignment04/Branch.java
@@ -1,14 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package assignment04;
 
-/**
- *
- * @author Gabriel
- */
 public class Branch {
-    
+    private String branch_name;
+    private String location;
+
+    public Branch(String branch_name, String location) {
+        setBranchName(branch_name);
+        setLocation(location);
+    }
+
+    private void setBranchName(String branch_name) {
+        this.branch_name = branch_name;
+    }
+
+    public String getBranchName() {
+        return branch_name;
+    }
+
+    private void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
 }
