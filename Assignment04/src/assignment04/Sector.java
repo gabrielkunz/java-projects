ee1e146c6caa961be3e18de11ac5bc3a057d5113
Gabriel Kunz <GabrielKunz97@gmail.com>
2021-06-09 23:54:28 -0300

    Update Sector.java

--- Assignment04/src/assignment04/Sector.java
+++ Assignment04/src/assignment04/Sector.java
@@ -1,5 +1,27 @@
 package assignment04;
 
 public class Sector {
+    private String sector_name;
+    private Branch branch;
 
+    public Sector(String sector_name, Branch branch){
+        setSectorName(sector_name);
+        setBranch(branch);
+    }
+
+    public void setSectorName(String sector_name) {
+        this.sector_name = sector_name;
+    }
+
+    public String getSectorName() {
+        return sector_name;
+    }
+
+    public void setBranch(Branch branch) {
+        this.branch = branch;
+    }
+
+    public Branch getBranch() {
+        return branch;
+    }
 }
