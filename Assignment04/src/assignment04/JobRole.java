47ec8d4b2670a554061cce22a9dd01f203f9f527
Gabriel Kunz <GabrielKunz97@gmail.com>
2021-06-09 23:54:21 -0300

    Update JobRole.java

--- Assignment04/src/assignment04/JobRole.java
+++ Assignment04/src/assignment04/JobRole.java
@@ -1,5 +1,27 @@
 package assignment04;
 
 public class JobRole {
+    private String role_name;
+    private Department department;
 
+    public JobRole(String role_name, Department department){
+        setRoleName(role_name);
+        setDepartment(department);
+    }
+
+    public void setRoleName(String role_name) {
+        this.role_name = role_name;
+    }
+
+    public String getRoleName() {
+        return role_name;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
 }
