347b8925a27909aba1e68cb21dfddee711991706
Gabriel Kunz <GabrielKunz97@gmail.com>
2021-07-05 18:50:42 -0300

    feat(JobRole): check if role requires cnh

--- Assignment04/src/assignment04/JobRole.java
+++ Assignment04/src/assignment04/JobRole.java
@@ -3,10 +3,12 @@ package assignment04;
 public class JobRole {
     private String role_name;
     private Department department;
+    private Boolean CNH_required;
 
-    public JobRole(String role_name, Department department){
+    public JobRole(String role_name, Department department, Boolean CNH_required){
         setRoleName(role_name);
         setDepartment(department);
+        setCNHRequired(CNH_required);
     }
 
     public void setRoleName(String role_name) {
@@ -23,5 +25,13 @@ public class JobRole {
 
     public Department getDepartment() {
         return department;
+    }
+
+    public void setCNHRequired(Boolean CNH_required) {
+        this.CNH_required = CNH_required;
+    }
+
+    public Boolean getCNHRequired() {
+        return CNH_required;
     }
 }
